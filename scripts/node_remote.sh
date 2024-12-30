24b67e72b852c830556b297edec56b19a2484d43
Ralf Sieger <sieger@fzi.de>
2024-12-30 19:44:15 +0100

    added workaround for cri-o on remote nodes to be able to pull images from unsecure private registry

--- scripts/node_remote.sh
+++ scripts/node_remote.sh
@@ -9,5 +9,12 @@ script_path="/vagrant/scripts"
 /bin/bash $script_path/node.sh
 
 sudo -i -u vagrant bash << EOF
-kubectl taint nodes $(hostname -s) remote=true:NoExecute
+kubectl taint nodes $(hostname -s) remote=true:NoExecute --overwrite=true
 EOF
+
+cat << EOF >> /etc/containers/registries.conf
+[[registry]]
+location = "10.0.0.10:30500"
+insecure = true
+EOF
+systemctl restart crio
