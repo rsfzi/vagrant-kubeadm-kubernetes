91a7adfdfb3883fd552fe24f7d1d14b96bdb5b29
Ralf Sieger <sieger@fzi.de>
2025-01-14 13:47:22 +0100

    removed debug

--- extract/extract.sh
+++ extract/extract.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir=`dirname $(readlink -f "$0")`
-echo script dir: $script_dir
+#echo script dir: $script_dir
 activate=$script_dir/venv/bin/activate
 . $activate
 py=$script_dir/main.py
