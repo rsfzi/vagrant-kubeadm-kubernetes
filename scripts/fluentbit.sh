ff52cca5da5d48651ae6b26be2c11ff7c7997b8e
Ralf Sieger <sieger@fzi.de>
2025-01-22 11:15:06 +0100

    moved to logging namespace

--- scripts/fluentbit.sh
+++ scripts/fluentbit.sh
@@ -4,6 +4,6 @@ set -euxo pipefail
 
 echo "Install fluentbit logging..."
 helm repo add fluent https://fluent.github.io/helm-charts
-helm -n fluent upgrade --install --create-namespace fluent-bit fluent/fluent-bit -f /vagrant/fluentbit_values.yaml
+helm -n logging upgrade --install --create-namespace fluent-bit fluent/fluent-bit -f /vagrant/fluentbit_values.yaml
 
 # helm -n fluent uninstall fluent-bit
