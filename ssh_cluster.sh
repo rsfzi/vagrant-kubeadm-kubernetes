fc06bcdf279058edb852b7e137465ccec33a938b
Ralf Sieger <sieger@fzi.de>
2025-06-26 08:57:04 +0200

    added local port forwards, too

--- ssh_cluster.sh
+++ ssh_cluster.sh
@@ -3,12 +3,16 @@
 HOST=se-ginkgo
 
 PORTS=(6443 32000 32001 30300 30500 30090 32080)
-#PORTS+=(8080)
+LOCAL_PORTS=(8080)
 FORWARDS=""
 
 for port in "${PORTS[@]}"
 do
     FORWARDS+=" -L ${port}:10.0.0.10:${port}"
+done
+for port in "${LOCAL_PORTS[@]}"
+do
+    FORWARDS+=" -L ${port}:127.0.0.1:${port}"
 done
 #echo $FORWARDS
 
